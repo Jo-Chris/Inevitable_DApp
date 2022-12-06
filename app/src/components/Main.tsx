ab62d1db81b385c63809e75820683718a15254fe
Chris <christof.jori.dev@gmail.com>
2022-12-06 16:55:49 +0100

    final

--- app/src/components/Main.tsx
+++ app/src/components/Main.tsx
@@ -1,5 +1,36 @@
+import {useCalls, useContractFunction, useEtherBalance, useEthers} from "@usedapp/core";
+import {formatEther} from "ethers/lib/utils";
+import {Contract, utils} from "ethers";
 
 export function Main() {
+
+    // get information from wallet
+    const {account, chainId} = useEthers();
+    const balance = useEtherBalance(account);
+
+    // call read function on SC
+    const cInterface = new utils.Interface(require('../contract/abi.json'));
+    const cAddr = '0x74B6485289e55EcEA94dEF49B25ced097beAFF14';
+    const contract = new Contract(cAddr, cInterface);
+
+    // calling readonly functions
+    const useMultiCalls = (calls: any[]) => {
+        const callsResult = useCalls(calls);
+        return callsResult.map(result => result ? result.value : null);
+    }
+    const [owner, name, symbol, balanceOf] = useMultiCalls([
+        {contract: contract, method: 'owner', args: []},
+        {contract: contract, method: 'name', args: []},
+        {contract: contract, method: 'symbol', args: []},
+        {contract: contract, method: 'balanceOf', args: [account]},
+    ]);
+
+    // call the mint function
+    const {
+        state: mintState,
+        send: mint
+    } = useContractFunction(contract, 'safeMint');
+
     return (
         <div className="section-container">
             <div className="section" id="sec-one">
@@ -8,17 +39,17 @@ export function Main() {
                     <div className="data-container">
                         <p>Address</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p className="addr">{account ?? 'n/v'}</p>
                     </div>
                     <div className="data-container">
                         <p>Chain ID</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p>{chainId ?? 'n/v'}</p>
                     </div>
                     <div className="data-container">
                         <p>Balance</p>
                         <hr/>
-                        <p>{}</p>
+                        <p>{balance ? formatEther(balance).slice(0, 7) + ' Goerli ETH' : 'n/v'}</p>
                     </div>
                 </div>
             </div>
@@ -28,28 +59,38 @@ export function Main() {
                     <div className="data-container">
                         <p>Owner</p>
                         <hr/>
-                        <p>{}</p>
+                        <p className="addr">{owner ?? 'fetch error!'}</p>
                     </div>
                     <div className="data-container">
                         <p>Name</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p>{name ?? 'fetch error'}</p>
                     </div>
                     <div className="data-container">
                         <p>Symbol</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p>{symbol ?? 'fetch error'}</p>
                     </div>
                     <div className="data-container">
                         <p>Mint Status</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p>{mintState.status}</p>
                     </div>
                     {
-                        <div className="data-container">
-                            <button className="btn btn-dark">Mint 🚀</button>
+                        account && <div className="data-container">
+                            <button
+                                className="btn btn-dark"
+                                onClick={() => mint(account, '/this-is-my-url')}
+                            >
+                                Mint 🚀
+                            </button>
                         </div>
                     }
+                    <div className="data-container">
+                        <p>Connected Wallet successful mints</p>
+                        <hr/>
+                        <p>{Number(balanceOf) ?? 'Fetch error!'}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,10 +1,24 @@
+import {useEthers} from "@usedapp/core";
 
 export function Navbar() {
+
+    const {account, activateBrowserWallet, deactivate} = useEthers();
+
+    const handleConnection = () => {
+        if (account) {
+            deactivate();
+        } else {
+            activateBrowserWallet();
+        }
+    }
 
     return (
         <nav className="nav">
             <h1>Smart Contracts on the Frontend</h1>
-            <button className="btn btn-dark">
+            <button className="btn btn-dark" onClick={() => handleConnection()}>
+                {
+                    account ? 'Deactivate' : 'Connect'
+                }
             </button>
         </nav>
     )

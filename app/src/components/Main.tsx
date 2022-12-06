528599d699b363de06c686c64fef3d02e6c51b5b
Chris <christof.jori.dev@gmail.com>
2022-12-06 15:01:41 +0100

    final

--- app/src/components/Main.tsx
+++ app/src/components/Main.tsx
@@ -1,5 +1,34 @@
+import {useCalls, useContractFunction, useEtherBalance, useEthers} from "@usedapp/core";
+import {formatEther} from "ethers/lib/utils";
+import {Contract, utils} from "ethers";
 
 export function Main() {
+
+    // this is the wallet section
+    const {account, chainId} = useEthers();
+    const balance = useEtherBalance(account);
+
+    // create our contract instance
+    const cInterface = new utils.Interface(require('../contract/abi.json'));
+    const cAddr = '0x54ab7e76e90892b27a65Bd01FBa39232B83bD568';
+    const contract = new Contract(cAddr, cInterface);
+
+    // custom hook for multi calls
+    const useMultiCalls = (calls: any[]) => {
+        const callResult = useCalls(calls);
+        return callResult.map(callResult => callResult ? callResult.value : null);
+    }
+
+    const [owner, name, symbol, balanceOf] = useMultiCalls([
+            {contract, method: 'owner', args: []},
+            {contract, method: 'name', args: []},
+            {contract, method: 'symbol', args: []},
+            {contract, method: 'balanceOf', args: [account]},
+        ]
+    )
+
+    const {state, send: mint} = useContractFunction(contract, 'safeMint');
+
     return (
         <div className="section-container">
             <div className="section" id="sec-one">
@@ -8,17 +37,17 @@ export function Main() {
                     <div className="data-container">
                         <p>Address</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p className='addr'>{account ?? 'n/v'}</p>
                     </div>
                     <div className="data-container">
                         <p>Chain ID</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p>{chainId ?? 'n/v'}</p>
                     </div>
                     <div className="data-container">
                         <p>Balance</p>
                         <hr/>
-                        <p>{}</p>
+                        <p>{balance ? formatEther(balance).slice(0, 7) + ' ETH' : 'n/v'}</p>
                     </div>
                 </div>
             </div>
@@ -28,28 +57,33 @@ export function Main() {
                     <div className="data-container">
                         <p>Owner</p>
                         <hr/>
-                        <p>{}</p>
+                        <p className="addr">{owner ?? 'fetch error'}</p>
                     </div>
                     <div className="data-container">
                         <p>Name</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p>{name ?? 'fetch error'}</p>
                     </div>
                     <div className="data-container">
                         <p>Symbol</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p>{symbol ?? 'fetch error'}</p>
                     </div>
                     <div className="data-container">
                         <p>Mint Status</p>
                         <hr/>
-                        <p>{'n/v'}</p>
+                        <p>{state.status}</p>
                     </div>
                     {
-                        <div className="data-container">
-                            <button className="btn btn-dark">Mint 🚀</button>
+                        account && <div className="data-container">
+                            <button className="btn btn-dark" onClick={() => mint(account, '/this-is-my-link-to-it')}>Mint 🚀</button>
                         </div>
                     }
+                    <div className="data-container">
+                        <p>Amount minted: </p>
+                        <hr/>
+                        <p>{Number(balanceOf) ?? 'fetch error'}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -5,14 +5,23 @@ import './App.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import {Config, DAppProvider, Goerli} from "@usedapp/core";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
+const config: Config = {
+    readOnlyChainId: Goerli.chainId,
+    readOnlyUrls: {
+        [Goerli.chainId]: 'https://goerli.infura.io/v3/51eb184b21c24b54a5d001444d4a8f4d',
+    },
+}
 
 root.render(
+    <DAppProvider config={config}>
         <App/>
+    </DAppProvider>
 );
 
 reportWebVitals();
